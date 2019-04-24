cf16b216e3c7c5bd25abdc280fbf57ed8e30654d
Anibal <anibal.eduardo@protonmail.ch>
2019-04-24 18:56:10 -0400

    wrap provider to ethers plus enforce kovan testnet

--- client/src/components/homeButton.js
+++ client/src/components/homeButton.js
@@ -1,18 +1,12 @@
 import React from 'react';
 import Button from 'react-bootstrap/Button';
 import { withRouter } from 'react-router-dom';
-import provider from '../utils/provider';
 
 class HomeButton extends React.Component {
   constructor(props, context) {
     super(props, context);
 
     this.handleClick = this.handleClick.bind(this);
-
-  }
-
-  componentDidMount() {
-    provider.metamask()
   }
 
   handleClick() {
--- client/src/components/navBar.js
+++ client/src/components/navBar.js
@@ -1,32 +1,47 @@
 import React from 'react';
 import { withRouter } from 'react-router-dom';
-import { Container, Navbar, Nav } from 'react-bootstrap';
+import { Container, Navbar, Nav, Alert } from 'react-bootstrap';
 import AaveImg from '../images/AAVE.png';
+import metamask from '../utils/provider';
+import { providers } from 'ethers';
 
 class NavBar extends React.Component {
+    constructor(props, context) {
+        super(props, context);
 
-  render() {
+        this.state = {
+            network: false,
+        };
+    }
+    componentWillMount() {
+        metamask();
+        let provider = new providers.Web3Provider(window.web3.currentProvider);
+        provider.getNetwork().then(currentNetwork => this.setState({ network: currentNetwork.name !== "kovan" ? true : false }) );
+    }
 
-    return (
-        <Container>
-            <Navbar bg="dark" variant="dark">
-                <Navbar.Brand href="https://aave.com/">
-		    	    <img
-        		        alt="AAVE"
-        		        src={AaveImg}
-        		        width="90"
-        		        height="30"
-        		        className="d-inline-block align-top"
-      		        />
-			    </Navbar.Brand>
-			    <Nav variant="tabs" className="justify-content-end">
-     			    <Nav.Link href="/" >APP </Nav.Link>
-      			    <Nav.Link href="/markets" >MARKETS</Nav.Link>
-    		    </Nav>
-            </Navbar>
-        </Container>
-    );
-  }
+    render() {
+
+        return (
+            <Container>
+                { this.state.network ? <Alert variant="dark">Aave Lending Pool is currently only available on Kovan Testnet</Alert> : null }  
+                <Navbar bg="dark" variant="dark">
+                    <Navbar.Brand href="https://aave.com/">
+		    	        <img
+        		            alt="AAVE"
+        		            src={AaveImg}
+        		            width="90"
+        		            height="30"
+        		            className="d-inline-block align-top"
+      		            />
+			        </Navbar.Brand>
+			        <Nav variant="tabs" className="justify-content-end">
+     			        <Nav.Link href="/" >APP </Nav.Link>
+      			        <Nav.Link href="/markets" >MARKETS</Nav.Link>
+    		        </Nav>
+                </Navbar>
+            </Container>
+        );
+    }
 }
 
 export default withRouter(NavBar)
